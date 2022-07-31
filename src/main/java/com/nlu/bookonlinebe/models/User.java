00ccf9a5aed848fd3db125129d6219537b5b3822
vodothinhan <73949607+vodothinhan@users.noreply.github.com>
2022-07-31 10:10:17 +0700

    Update User.java

--- src/main/java/com/nlu/bookonlinebe/models/User.java
+++ src/main/java/com/nlu/bookonlinebe/models/User.java
@@ -39,4 +39,10 @@ public class User {
         this.status = status;
         this.avatar = avatar;
     }
+
+    public User(String fullname, String email, String password){
+        this.fullname = fullname;
+        this.email = email;
+        this.password = password;
+    }
 }
